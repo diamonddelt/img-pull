93cc7a47443873547a4f42e049ad970ace9fc832
diamonddelt <rrrasti@yahoo.com>
2018-11-19 14:31:07 -0600

    Add initial proof of concept of img-pull

--- TODO.md
+++ TODO.md
@@ -2,9 +2,18 @@
 
 ## Pending Tasks
 
-* implement POC using a simple main func
-* use the gocolly/colly library for scraping functionality
 * wrap the POC in the cobra framework
 * make 'datatype' a struct
+* modify imagePull() to download files to a specified dir
+* research why a request with no user agent gives a 403 forbidden
+* research how to properly use 'goquerySelector' for more advanced HTMLElement queries https://github.com/PuerkitoBio/goquery
+* there is a looping problem when downloading the images... figure out how to fix that. probably deals with the contexts or async
+    * the collector should spawn a new goroutine for each image it's about to download, instead of doing it sequentially
+    * the program works asynchronously with the c.Wait() calls, but the output is wonky... figure that out too
+* Update the LICENSE information to make it as restrictive as possible
+
 
 ## Completed Tasks
+
+* implement POC using a simple main func
+* use the gocolly/colly library for scraping functionality - http://go-colly.org/ 
--- cmd/img-pull/main.go
+++ cmd/img-pull/main.go
@@ -1,54 +1,92 @@
 // img-pull
 // Copyright 2018 Ryan Rasti
-// Refer to IDEAS.MD
+// All rights reserved
 
 package main
 
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	// ultimate test case: https://cultofthepartyparrot.com/
-	imagePull(".gif", "https://cultofthepartyparrot.com/")
+	imagePull(".gif", `C:\img-pull\`, "https://cultofthepartyparrot.com/") // test it out
 }
 
-// the workhorse
-// leverages the colly framework http://go-colly.org/
-// TODO: Remove me later
-// https://github.com/gocolly/colly/blob/master/_examples/basic/basic.go
-func imagePull(dataType string, domains ...string) error {
+func imagePull(dataType string, downloadDir string, domains ...string) error {
 	var err error
 
+	// basic collector
 	c := colly.NewCollector(
 		// setup domains to pull from
-		colly.AllowedDomains(domains...),
-	)
+		// colly.AllowedDomains(domains...),
 
-	// callback for each request
+		// set bogus user-agent
+		colly.UserAgent("xyz"),
+
+		colly.AllowURLRevisit(),
+
+		// execute collector requests asynchronously
+		colly.Async(true),
+	)
+	d := c.Clone() // duplicate collector to be used for downloading images
+
+	// logs for each request
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Collecting", dataType, "from", r.URL.String())
+		fmt.Println("Collecting all", dataType, "from", r.URL.String())
 	})
 
 	if dataType == ".gif" {
 		c.OnHTML("img[src]", func(e *colly.HTMLElement) {
 			src := e.Attr("src") // get the src attribute of the img
-			fmt.Printf("Image found with src: %s", src)
+			fmt.Printf("Image found with src: %s\n", src)
+
+			// handle image src with content hosted at FQDNs
+			if strings.Contains(src, "http") {
+				e.Request.Visit(src)
+				c.OnResponse(func(r *colly.Response) {
+					fmt.Printf("Navigated to direct image source!")
+				})
+			} else {
+				fmt.Println("Visiting absolute URI for image src", src, "at", e.Request.AbsoluteURL(src))
+				d.Visit(e.Request.AbsoluteURL(src))
+				d.OnResponse(func(r *colly.Response) {
+					downloadFileFromResponse(e.Request.AbsoluteURL(src), downloadDir, r)
+				})
+			}
 		})
 	}
 
 	for _, v := range domains {
+		fmt.Println("Visiting", v)
 
 		// collect data from each domain
 		err = c.Visit(v)
 		if err != nil {
-			l := fmt.Sprintf("error visiting %s", v)
+			l := fmt.Sprintf("error visiting %s ", v)
 			log.Fatal(l, err)
 			return err
 		}
+	}
+
+	// TODO: understand the async ordering better... the calls below execute in the wrong order
+	d.Wait() // wait until download aggregater collector threads are finished
+	c.Wait() // wait until main collector thread is finished
+
+	return nil
+}
+
+func downloadFileFromResponse(url string, dir string, r *colly.Response) error {
+	var err error
+
+	fmt.Println("Saving file at", dir+r.FileName())
+	err = r.Save(dir + r.FileName()) // FileName returns the sanitized file name parsed from "Content-Disposition" header or from URL
+	if err != nil {
+		log.Print("unable to save image from URL", url, "to filesystem location", dir)
+		return err
 	}
 
 	return nil
